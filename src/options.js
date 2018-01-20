95f3d246e4466e16cfc1924378a64609cea7cf08
Nathaniel Saul <nat@saulgill.com>
2018-01-19 20:36:49 -0800

    include an options page, does not incorporate them into threshold yet.

---
+++ src/options.js
@@ -0,0 +1,21 @@
+function saveOptions(e) {
+  browser.storage.sync.set({
+    threshold: document.querySelector("#threshold").value
+  });
+  e.preventDefault();
+}
+
+function restoreOptions() {
+  var storageItem = browser.storage.managed.get('threshold');
+  storageItem.then((res) => {
+    document.querySelector("#managed-threshold").innerText = res.threshold;
+  });
+
+  var gettingItem = browser.storage.sync.get('threshold');
+  gettingItem.then((res) => {
+    document.querySelector("#threshold").value = res.threshold || 'Firefox red';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
